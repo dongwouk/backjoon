8a359a293e532a2d098fe8e8cbe615e334096a62
kwon <kwondw30@gmail.com>
2025-03-13 20:51:09 +0900

    재귀

--- src/재귀/ex27433.java
+++ src/재귀/ex27433.java
@@ -1,7 +1,15 @@
 package 재귀;
 
+import java.util.Scanner;
+
 public class ex27433 {
 public static void main(String[] args) {
-	
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	long sum=1;
+	for (int i = 1; i <= n; i++) {
+		sum*=i;
+	}
+	System.out.println(sum);
 }
 }
